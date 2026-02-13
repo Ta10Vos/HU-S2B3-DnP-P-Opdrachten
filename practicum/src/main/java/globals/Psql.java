a068b824942118f222147cc3af7d4dfcc7dc3a5b
Ta10Vos <tim.vos@student.hu.nl>
2026-02-13 10:34:04 +0100

    Remove environment files and add to .gitignore

--- practicum/src/main/java/globals/Psql.java
+++
@@ -1,8 +0,0 @@
-package globals;
-
-public class Psql {
-
-    // Naam van de db voor psql-tests, ben je geheel vrij in. Want het testraamwerk tuigt de db op en af
-    public static final String dbName = "ovchippsql";
-
-}

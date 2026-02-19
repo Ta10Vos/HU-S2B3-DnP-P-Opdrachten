800f3c08ff01920cc1279a4d44b4f9dfd62d43de
Ta10Vos <tim.vos@student.hu.nl>
2026-02-19 19:34:30 +0100

    Reiziger geprogrammeerd

--- practicum/src/main/java/domain/Reiziger.java
+++ practicum/src/main/java/domain/Reiziger.java
@@ -3,69 +3,114 @@ package domain;
 import jakarta.persistence.*;
 
 import java.sql.Date;
-import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Reiziger {
+    private int id;
+    private String voorletters;
+    private String tussenvoegsel;
+    private String achternaam;
+    private Date geboortedatum;
+
+    private domain.Adres adres;
+    private List<OvChipkaart> ovChipkaarten = new ArrayList<OvChipkaart>();
 
     public Reiziger() {}
 
-    public String getVoorletters() {
-        return null;
-    }
-
-    public void setVoorletters(String voorletters) {
-
-    }
-
-    public String getTussenvoegsel() {
-        return null;
-    }
-
-    public void setTussenvoegsel(String tussenvoegsel) {
-
-    }
-
-    public String getAchternaam() {
-        return null;
-    }
-
-    public void setAchternaam(String achternaam) {
-
-    }
-
-    public String ToString() {
-        return null;
-    }
-
     public int getReizigerId() {
-        return 0;
+        return id;
     }
 
     public void setReizigerId(int reizigerId) {
+        id = reizigerId;
+    }
 
+    public String getVoorletters() { return voorletters; }
+
+    public void setVoorletters(String voorletters) {
+        this.voorletters = voorletters.trim();
+    }
+
+    public String getTussenvoegsel() {
+        return tussenvoegsel;
+    }
+
+    public void setTussenvoegsel(String tussenvoegsel) {
+        if (tussenvoegsel == null) {
+            tussenvoegsel = "";
+        }
+        this.tussenvoegsel = tussenvoegsel.trim();
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam.trim();
     }
 
     public Date getGeboortedatum() {
-        return null;
+        return geboortedatum;
     }
 
     public void setGeboortedatum(Date geboortedatum) {
-
+        this.geboortedatum = geboortedatum;
     }
 
     public domain.Adres getAdres() {
-        return null;
+        return this.adres;
     }
 
     public void setAdres(domain.Adres adres) {
-
+        this.adres = adres;
     }
 
     public List<OvChipkaart> getOvChipkaart() {
         return null;
     }
 
-    public void setOvChipkaart(List<OvChipkaart> ovChipkaart) {
+    public void setOvChipkaart(List<OvChipkaart> ovChipkaarten) {
+        this.ovChipkaarten = ovChipkaarten;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        List<OvChipkaart> ovChipkaarten = getOvChipkaart();
+
+        sb.append("Student ");
+        sb.append(getVoorletters());
+
+        if (!getTussenvoegsel().isEmpty()) {
+            sb.append(" ");
+            sb.append(getTussenvoegsel());
+        }
+
+        sb.append(" ");
+        sb.append(getAchternaam());
+
+        if (!(getGeboortedatum() == null)) {
+            sb.append(" is geboren op ");
+            sb.append(getGeboortedatum().toString());
+        }
+
+        if (!(getAdres() == null)) {
+            sb.append(" woont op: ");
+            sb.append(getAdres().toString());
+        }
+
+        if (!ovChipkaarten.isEmpty()) {
+            sb.append(" en heeft ov-chipkaarten: \n");
+
+            for(OvChipkaart ovChip : getOvChipkaart()) {
+                sb.append(ovChip.toString());
+                sb.append("\n");
+            }
+        }
+
+        return sb.toString();
     }
 }

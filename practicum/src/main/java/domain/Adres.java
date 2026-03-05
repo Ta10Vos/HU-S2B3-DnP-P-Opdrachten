b484c66cab88bf026941f9d8b18eca64d9eb4f31
Ta10Vos <tim.vos@student.hu.nl>
2026-03-05 14:49:09 +0100

    Implemented Adres.java

--- practicum/src/main/java/domain/Adres.java
+++ practicum/src/main/java/domain/Adres.java
@@ -1,53 +1,67 @@
 package domain;
 
 public class Adres {
+    private int id;// max 10
+    private String postcode;// max 10
+    private String huisnummer;// max 10
+    private String straat;// max 255
+    private String woonplaats;// max 255
 
     private Reiziger reiziger;
 
     public int getAdresId() {
-        return 0;
+        return id;
     }
 
     public void setAdresId(int adresId) {
-
+        this.id = adresId;
     }
 
     public String getPostcode() {
-        return null;
+        return postcode;
     }
 
+    /** Set the postcode with the max of 10 chars */
     public void setPostcode(String postcode) {
-
+        if (postcode.length() > 10) return;
+        this.postcode = postcode;
     }
 
     public String getHuisnummer() {
-        return null;
+        return postcode;
     }
 
+    /** Set the huisnummer with the max of 10 chars */
     public void setHuisnummer(String huisnummer) {
-
+        if (huisnummer.length() > 10) return;
+        this.huisnummer = huisnummer;
     }
 
     public String getStraat() {
-        return null;
+        return straat;
     }
 
+    /** Set the straat with the max of 255 chars */
     public void setStraat(String straat) {
-
+        if (straat.length() > 255) return;
+        this.straat = straat;
     }
 
     public String getWoonplaats() {
-        return null;
+        return woonplaats;
     }
 
+    /** Set the woonplaats with the max of 255 chars */
     public void setWoonplaats(String woonplaats) {
-
+        if (woonplaats.length() > 255) return;
+        this.woonplaats = woonplaats;
     }
 
     public Reiziger getReiziger() {
-        return null;
+        return reiziger;
     }
 
     public void setReiziger(Reiziger reiziger) {
+        this.reiziger = reiziger;
     }
 }

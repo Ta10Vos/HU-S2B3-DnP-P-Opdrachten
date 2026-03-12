298a5e11da6a0d6fa682aceb734408661088170c
Ta10Vos <tim.vos@student.hu.nl>
2026-03-12 20:38:28 +0100

    Implemneted basic select logic

--- practicum/src/main/java/infra/dao/OvChipkaartDaoPsql.java
+++ practicum/src/main/java/infra/dao/OvChipkaartDaoPsql.java
@@ -3,6 +3,7 @@ package infra.dao;
 import domain.*;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OvChipkaartDaoPsql implements IOvChipkaartDao {
@@ -24,7 +25,7 @@ public class OvChipkaartDaoPsql implements IOvChipkaartDao {
 
         pst.setInt(1, ovChipkaart.getKaartNummer());
         pst.setDate(2, ovChipkaart.getGeldigTot());
-        pst.setLong(3, ovChipkaart.getKlasse());
+//        pst.setLong(3, ovChipkaart.getKlasse());
         pst.setBigDecimal(4, ovChipkaart.getSaldo());
         pst.setInt(5, rzgId);
 
@@ -50,7 +51,7 @@ public class OvChipkaartDaoPsql implements IOvChipkaartDao {
         int rzgId = ovChipkaart.getReiziger().getReizigerId();
 
         pst.setDate(1, ovChipkaart.getGeldigTot());
-        pst.setLong(2, ovChipkaart.getKlasse());
+//        pst.setLong(2, ovChipkaart.getKlasse());
         pst.setBigDecimal(3, ovChipkaart.getSaldo());
         pst.setInt(4, rzgId);
         pst.setInt(5, ovChipkaart.getKaartNummer());
@@ -78,17 +79,104 @@ public class OvChipkaartDaoPsql implements IOvChipkaartDao {
 
     @Override
     public OvChipkaart findById(int id) throws SQLException {
-        return null;
+        String sql = "SELECT " +
+                "kaart_nummer, " +
+                "geldig_tot, " +
+                "klasse, " +
+                "saldo, " +
+                "reiziger_id " +
+                "FROM ov_chipkaart " +
+                "WHERE kaart_nummer=?;";
+
+        PreparedStatement pst = connection.prepareStatement(sql);
+
+        pst.setInt(1, id);
+
+        if (!pst.execute()) return null;
+
+        ResultSet rs = pst.getResultSet();
+
+        if (!rs.next()) return null;
+
+        OvChipkaart ock = new OvChipkaart();
+
+        ock.setKaartNummer(rs.getInt("kaart_nummer"));
+        ock.setGeldigTot(rs.getDate("geldig_tot"));
+//        ock.setKlasse(rs.getInt("klasse"));
+        ock.setSaldo(rs.getBigDecimal("saldo"));
+
+        rs.close();
+        pst.close();
+        return ock;
     }
 
     @Override
     public List<OvChipkaart> findByReiziger(Reiziger reiziger) throws SQLException {
-        return null;
+        String sql = "SELECT " +
+                "kaart_nummer, " +
+                "geldig_tot, " +
+                "klasse, " +
+                "saldo, " +
+                "reiziger_id " +
+                "FROM ov_chipkaart " +
+                "WHERE reiziger_id=?;";
+
+        PreparedStatement pst = connection.prepareStatement(sql);
+
+        pst.setInt(1, reiziger.getReizigerId());
+
+        if (!pst.execute()) return null;
+
+        ResultSet rs = pst.getResultSet();
+        List<OvChipkaart> ovChipkaarten = new ArrayList<OvChipkaart>();
+
+        while(rs.next()) {
+            OvChipkaart ock = new OvChipkaart();
+
+            ock.setKaartNummer(rs.getInt("kaart_nummer"));
+            ock.setGeldigTot(rs.getDate("geldig_tot"));
+//            ock.setKlasse(rs.getInt("klasse"));
+            ock.setSaldo(rs.getBigDecimal("saldo"));
+
+            ovChipkaarten.add(ock);
+        }
+
+        rs.close();
+        pst.close();
+        return ovChipkaarten;
     }
 
     @Override
     public List<OvChipkaart> findAll() throws SQLException {
-        return null;
+        String sql = "SELECT " +
+                "kaart_nummer, " +
+                "geldig_tot, " +
+                "klasse, " +
+                "saldo, " +
+                "reiziger_id " +
+                "FROM ov_chipkaart;";
+
+        PreparedStatement pst = connection.prepareStatement(sql);
+
+        if (!pst.execute()) return null;
+
+        ResultSet rs = pst.getResultSet();
+        List<OvChipkaart> ovChipkaarten = new ArrayList<OvChipkaart>();
+
+        while(rs.next()) {
+            OvChipkaart ock = new OvChipkaart();
+
+            ock.setKaartNummer(rs.getInt("kaart_nummer"));
+            ock.setGeldigTot(rs.getDate("geldig_tot"));
+//            ock.setKlasse(rs.getInt("klasse"));
+            ock.setSaldo(rs.getBigDecimal("saldo"));
+
+            ovChipkaarten.add(ock);
+        }
+
+        rs.close();
+        pst.close();
+        return ovChipkaarten;
     }
 
     public void setProductDao(IProductDao productDao) {

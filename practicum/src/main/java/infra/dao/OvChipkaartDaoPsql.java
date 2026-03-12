b4f64ae103a44123ed44b4a47da977f90698b88f
Ta10Vos <tim.vos@student.hu.nl>
2026-03-12 20:27:24 +0100

    Implemented CREATE, DELETE, UPDATE

--- practicum/src/main/java/infra/dao/OvChipkaartDaoPsql.java
+++ practicum/src/main/java/infra/dao/OvChipkaartDaoPsql.java
@@ -14,14 +14,66 @@ public class OvChipkaartDaoPsql implements IOvChipkaartDao {
     }
     @Override
     public boolean save(OvChipkaart ovChipkaart) throws SQLException {
+        String sql = "INSERT INT0 " +
+                "ov_chipkaart(kaart_nummer, geldig_tot, klasse, saldo, reiziger_id) " +
+                "VALUES (?, ?, ?, ?, ?);";
+
+        PreparedStatement pst = connection.prepareStatement(sql);
+
+        int rzgId = ovChipkaart.getReiziger().getReizigerId();
+
+        pst.setInt(1, ovChipkaart.getKaartNummer());
+        pst.setDate(2, ovChipkaart.getGeldigTot());
+        pst.setLong(3, ovChipkaart.getKlasse());
+        pst.setBigDecimal(4, ovChipkaart.getSaldo());
+        pst.setInt(5, rzgId);
+
+        // Check if at least 1 row action was executed
+        boolean result = pst.executeUpdate() > 0;
+
+        pst.close();
+        return result;
     }
 
     @Override
     public boolean update(OvChipkaart ovChipkaart) throws SQLException {
+        String sql = "UPDATE ov_chipkaart " +
+                "SET" +
+                "geldig_tot=?, " +
+                "klasse=?, " +
+                "saldo=?, " +
+                "reiziger_id=? " +
+                "WHERE kaart_nummer=?;";
+
+        PreparedStatement pst = connection.prepareStatement(sql);
+
+        int rzgId = ovChipkaart.getReiziger().getReizigerId();
+
+        pst.setDate(1, ovChipkaart.getGeldigTot());
+        pst.setLong(2, ovChipkaart.getKlasse());
+        pst.setBigDecimal(3, ovChipkaart.getSaldo());
+        pst.setInt(4, rzgId);
+        pst.setInt(5, ovChipkaart.getKaartNummer());
+
+        // Check if at least 1 row action was executed
+        boolean result = pst.executeUpdate() > 0;
+
+        pst.close();
+        return result;
     }
 
     @Override
     public boolean delete(OvChipkaart ovChipkaart) throws SQLException {
+        String sql = "DELETE FROM ov_chipkaart " +
+                "WHERE kaart_nummer=?;";
+
+        PreparedStatement pst = connection.prepareStatement(sql);
+        pst.setInt(1, ovChipkaart.getKaartNummer());
+
+        boolean result = pst.executeUpdate() > 0;
+
+        pst.close();
+        return result;
     }
 
     @Override
